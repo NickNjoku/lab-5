cdf2316a77da57ff19e6751017122abf08baa6f4
Nick Njoku <nnjoku245@ou.edu>
2019-02-19 15:46:41 -0600

    changed Todo times for task

--- Java 2 - Lab 5/src/Todo.java
+++ Java 2 - Lab 5/src/Todo.java
@@ -5,17 +5,20 @@ OU ID_______________: 113437448
 OU EMAIL____________: nnjoku245@ou.edu
 
 Tasks:
-1. Finish calculateTwoTokens method
-2. Finish calculateThreeTokens method
-3. Finish CalculatorTest class
+1. Finish Calculator class
+2. Finish calculateTwoTokens method
+3. Finish calculateThreeTokens method
+4. Finish CalculatorTest class
 
 Estimated Time (minutes):
-1. 15
-2. 10
-3. 30
+1. 120
+2. 15
+3. 10
+4. 30
 
 Actual Time (minutes):
-1. 12
-2. 10
-3. 30
+1. 180
+2. 12
+3. 10
+4. 40
 */}
