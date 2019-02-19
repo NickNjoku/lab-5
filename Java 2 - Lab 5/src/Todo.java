2afc81d23e28e298869e7c28d24e9904f3e983d8
Nick Njoku <nnjoku245@ou.edu>
2019-02-19 13:29:44 -0600

    first commit - Calculator class complete, working on Test

---
+++ Java 2 - Lab 5/src/Todo.java
@@ -0,0 +1,21 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-12
+OU ID_______________: 113437448
+OU EMAIL____________: nnjoku245@ou.edu
+
+Tasks:
+1. Finish calculateTwoTokens method
+2. Finish calculateThreeTokens method
+3. Finish CalculatorTest class
+
+Estimated Time (minutes):
+1. 15
+2. 10
+3. 30
+
+Actual Time (minutes):
+1. 12
+2. 10
+3. 30
+*/}
